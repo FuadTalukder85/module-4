a669fb416b89b4d3159948ce4d1c621a771412ef
Fuad Talukder <webfuadtk@gmail.com>
2024-01-16 18:27:44 +0600

    complete module 5-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { FormEvent, useState } from "react";
-import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
-import Modal from "./components/ui/Modal";
+
+import NormalForm from "./components/NormalForm/NormalForm";
 
 function App() {
-  const [modal, setModal] = useState(false);
-
-  const handleModalClose = () => {
-    setModal((prev) => !prev);
-  };
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log("clicked");
-  };
-
   return (
     <Container>
-      <div className="h-screen w-full flex justify-center items-center">
-        <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
-        <Modal isOpen={modal} onClose={handleModalClose}>
-          <Modal.Header>
-            <h1>This is modal title</h1>
-            <Modal.CloseButton></Modal.CloseButton>
-          </Modal.Header>
-          <form onSubmit={handleSubmit}>
-            <input type="text" />
-            <button type="submit">Submit</button>
-          </form>
-        </Modal>
-      </div>
+      <NormalForm></NormalForm>
     </Container>
   );
 }
---
+++ src/components/NormalForm/NormalForm.tsx
@@ -0,0 +1,85 @@
+import { useForm } from "react-hook-form";
+import cn from "../../utils/cn";
+
+const NormalForm = () => {
+  const { register, handleSubmit } = useForm();
+  const onsubmit = (data) => {
+    console.log(data);
+  };
+
+  const double = true;
+
+  return (
+    <form
+      onSubmit={handleSubmit(onsubmit)}
+      className={cn("border border-red-500 w-full p-5 mx-auto", {
+        "max-w-5xl": double,
+        "max-w-md": !double,
+      })}
+    >
+      <div
+        className={cn(
+          "border border-blue-500 grid grid-cols-1 justify-items-center gap-5",
+          {
+            "md:grid-cols-2": double,
+          }
+        )}
+      >
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Name
+          </label>
+          <input type="text" id="name" {...register("name")} />
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Email
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            id="email"
+            {...register("email")}
+          />
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Password
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            id="password"
+            {...register("password")}
+          />
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Email
+          </label>
+          <select>
+            <option>one</option>
+            <option>two</option>
+            <option>three</option>
+            <option>four</option>
+          </select>
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Email
+          </label>
+          <textarea></textarea>
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Email
+          </label>
+          <input type="checkbox" />
+        </div>
+      </div>
+      {/* <button type="submit">Submit</button> */}
+    </form>
+  );
+};
+
+export default NormalForm;
