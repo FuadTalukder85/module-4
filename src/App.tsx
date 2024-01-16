246432d35e505450ea8090b7222bee6823f80812
Fuad Talukder <webfuadtk@gmail.com>
2024-01-16 15:45:23 +0600

    complete module-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { FormEvent, useState } from "react";
 import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
+import Modal from "./components/ui/Modal";
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const handleModalClose = () => {
+    setModal((prev) => !prev);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
-        <Button className="" outline={true}></Button>
+        <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
+        <Modal isOpen={modal} onClose={handleModalClose}>
+          <Modal.Header>
+            <h1>This is modal title</h1>
+            <Modal.CloseButton></Modal.CloseButton>
+          </Modal.Header>
+          <form onSubmit={handleSubmit}>
+            <input type="text" />
+            <button type="submit">Submit</button>
+          </form>
+        </Modal>
       </div>
     </Container>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,25 +1,40 @@
-// import { twMerge } from "tailwind-merge";
-// import { clsx } from "clsx";
+import { DetailedHTMLProps, HTMLAttributes, forwardRef } from "react";
 import cn from "../../utils/cn";
 
-// `bg-purple-500 px-3 py-2 rounded-md ${className} ${
-//     variant == "outline" ? "border border-purple-400 bg-opacity-10" : ""
-//   }`
-
-const Button = ({ className, outline }) => {
-  return (
-    <button
-      className={cn(
-        "bg-purple-500 px-3 py-2 rounded-md",
-        {
-          "border border-purple-400 bg-opacity-10": outline,
-        },
-        className
-      )}
-    >
-      Click
-    </button>
-  );
+type TRef = HTMLButtonElement;
+type TVariant = "solid" | "ghost" | "outline";
+type TButtonOptions = {
+  variant?: TVariant;
 };
+type TButton = DetailedHTMLProps<
+  HTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  TButtonOptions;
+
+const Button = forwardRef<TRef, TButton>(
+  ({ className, variant = "solid", children, ...rest }, ref) => {
+    const getVariant = (variant: TVariant) => {
+      switch (variant) {
+        case "outline":
+          return "btn-outline";
+        case "ghost":
+          return "btn-ghost";
+        default:
+          return "btn-solid";
+      }
+    };
+
+    return (
+      <button
+        {...rest}
+        ref={ref}
+        className={cn(getVariant(variant), className)}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
