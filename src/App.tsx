c9fd61e43465b023fbc030273ecc1aa6e564f0ed
Fuad Talukder <webfuadtk@gmail.com>
2024-01-18 18:35:40 +0600

    complete module 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
 import Container from "./components/ui/Container";
-
-import NormalForm from "./components/NormalForm/NormalForm";
+import {
+  Form,
+  FormSection,
+  FormSubmit,
+  Input,
+} from "./components/ReuseableForm";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
 function App() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<TTest>();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  const TestSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  });
+
+  type TTest = z.infer<typeof TestSchema>;
+
   return (
     <Container>
-      <NormalForm></NormalForm>
+      <Form onSubmit={handleSubmit(onSubmit) as SubmitHandler<FieldValues>}>
+        <FormSection>
+          <div className="w-full max-w-md">
+            <label className="block" htmlFor="name">
+              Name
+            </label>
+            <input type="text" id="name" {...register("name")} />
+            {errors.name && (
+              <span className="text-xs text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <Input
+            type="email"
+            register={register("email")}
+            errors={errors}
+            label="Email"
+          ></Input>
+        </FormSection>
+        <FormSubmit></FormSubmit>
+      </Form>
     </Container>
   );
 }
--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -1,80 +1,105 @@
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm, FormProvider } from "react-hook-form";
 import cn from "../../utils/cn";
 import Button from "../ui/Button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignUpSchema, TNormalForm } from "./validation";
+import InputFeild from "./InputFeild";
 
 const NormalForm = () => {
+  const methods = useForm<TNormalForm>({
+    resolver: zodResolver(SignUpSchema),
+  });
+
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
-  } = useForm();
-  const onsubmit = (data) => {
+  } = methods;
+
+  const onsubmit = (data: FieldValues) => {
     console.log(data);
   };
+
+  console.log(watch("name"));
 
   const double = true;
 
   return (
-    <form
-      onSubmit={handleSubmit(onsubmit)}
-      className={cn("border border-gray-300 rounded-lg w-full p-5 mx-auto", {
-        "max-w-5xl": double,
-        "max-w-md": !double,
-      })}
-    >
-      <div
-        className={cn("grid grid-cols-1 justify-items-center gap-5", {
-          "md:grid-cols-2": double,
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit(onsubmit)}
+        className={cn("border border-gray-300 rounded-lg w-full p-5 mx-auto", {
+          "max-w-5xl": double,
+          "max-w-md": !double,
         })}
       >
-        <div className="w-full max-w-md">
-          <label className="block" htmlFor="name">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            {...register("name", { required: true })}
-          />
-          {errors.name && (
-            <span className="text-xs text-red-500">This field is required</span>
-          )}
+        <div
+          className={cn("grid grid-cols-1 justify-items-center gap-5", {
+            "md:grid-cols-2": double,
+          })}
+        >
+          <div className="w-full max-w-md">
+            <label className="block" htmlFor="name">
+              Something
+            </label>
+            <InputFeild></InputFeild>
+          </div>
+          <div className="w-full max-w-md">
+            <label className="block" htmlFor="name">
+              Name
+            </label>
+            <input type="text" id="name" {...register("name")} />
+            {errors.name && (
+              <span className="text-xs text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full max-w-md">
+            <label className="block" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="w-full"
+              type="email"
+              id="email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <span className="text-xs text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full max-w-md">
+            <label className="block" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="w-full"
+              type="password"
+              id="password"
+              {...register("password", { minLength: 8 })}
+            />
+            {errors.password && (
+              <span className="text-xs text-red-500">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
         </div>
-        <div className="w-full max-w-md">
-          <label className="block" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="w-full"
-            type="email"
-            id="email"
-            {...register("email")}
-          />
+        <div
+          className={cn("grid grid-cols-1 justify-items-center gap-5 my-10", {
+            "md:grid-cols-2": double,
+          })}
+        >
+          <div className="w-full max-w-md col-start-1 md:col-start-2 flex justify-end">
+            <Button className="w-full md:w-fit">Submit</Button>
+          </div>
         </div>
-        <div className="w-full max-w-md">
-          <label className="block" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="w-full"
-            type="password"
-            id="password"
-            {...register("password")}
-          />
-        </div>
-      </div>
-      <div
-        className={cn("grid grid-cols-1 justify-items-center gap-5 my-10", {
-          "md:grid-cols-2": double,
-        })}
-      >
-        <div className="w-full max-w-md col-start-1 md:col-start-2 flex justify-end">
-          <Button className="w-full md:w-fit" type="submit">
-            Submit
-          </Button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   );
 };
 
